54c397e0aee9f810d1379f640a9f63cced5d1b3a
dana <dana.lenz@rwth-aachen.de>
2021-10-04 00:05:49 +0200

    state

--- Problem.h
+++ Problem.h
@@ -12,6 +12,7 @@
 #include "Assignment.h"
 #include "Rule.h"
 #include "Component.h"
+#include "Model.h"
 
 namespace omtsched {
 
@@ -46,6 +47,12 @@ namespace omtsched {
 
         void addRule(const Rule<ComponentID, GroupID, TagID> &);
 
+        //void solve(const bool &allModels);
+
+        void getModel(Model &model) const;
+
+        void printProblem(std::ostream) const;
+
     private:
 
         std::set<TagID> tags;
@@ -71,47 +78,9 @@ Problem(const std::string &name)
         : problemName(name) {};
 
 template<typename ComponentID, typename GroupID, typename TagID>
-omtsched::Problem<ComponentID, GroupID, TagID>::
-Problem() {};
-
-template<typename ComponentID, typename GroupID, typename TagID>
-void omtsched::Problem<ComponentID, GroupID, TagID>::addGroup(const GroupID &id) {
-
-    groups.insert(id);
-}
-
-template<typename ComponentID, typename GroupID, typename TagID>
-void omtsched::Problem<ComponentID, GroupID, TagID>::deleteGroup(const GroupID &id) {
-
-    for(auto &component : components)
-        component->removeGroup(id);
-
-    groups.erase(id);
-}
-
-template<typename ComponentID, typename GroupID, typename TagID>
 const std::set<GroupID> &omtsched::Problem<ComponentID, GroupID, TagID>::getAllGroups() const {
 
     return groups;
-}
-
-template<typename ComponentID, typename GroupID, typename TagID>
-void omtsched::Problem<ComponentID, GroupID, TagID>::addTag(const TagID &id) {
-
-    for(auto &component : components)
-        component->addTag(id);
-
-    tags.insert(id);
-
-}
-
-template<typename ComponentID, typename GroupID, typename TagID>
-void omtsched::Problem<ComponentID, GroupID, TagID>::deleteTag(const TagID &id) {
-
-    for(auto &component : components)
-        component->removeTag(id);
-
-    tags.erase(id);
 }
 
 template<typename ComponentID, typename GroupID, typename TagID>
@@ -148,9 +117,9 @@ void omtsched::Problem<ComponentID, GroupID, TagID>::addRule(const Rule<Componen
 }
 
 template<typename ComponentID, typename GroupID, typename TagID>
-void omtsched::Problem<ComponentID, GroupID, TagID>::addComponent(const Component<ComponentID, GroupID, TagID> &component) {
+void omtsched::Problem<ComponentID, GroupID, TagID>::getModel(Model &model) const {
 
-    components.push_back(std::make_unique<Component<ComponentID, GroupID, TagID>>(component));
+    //Step 1.
 }
 
 #endif //OMTSCHED_PROBLEM_H
